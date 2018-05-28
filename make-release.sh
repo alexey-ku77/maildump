62dffa3171ff33ec210803b9cd2a72621ab28402
alexey-ku77 <kuznetsov.alexxxey@gmail.com>
2018-05-28 15:27:45 +0300

    Commented out second rm command.
    So freshly generated assets are not removed.

--- make-release.sh
+++ make-release.sh
@@ -8,4 +8,4 @@ webassets -m maildump.web build --production
 # Create and update release
 # python setup.py sdist upload
 # Delete assets again, we don't need them anymore
-rm -rf maildump/static/.webassets-cache/ maildump/static/assets/bundle.*
+# rm -rf maildump/static/.webassets-cache/ maildump/static/assets/bundle.*
